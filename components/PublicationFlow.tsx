6094f57c8b22761591228354c8fbf309ecb3f870
Joel <joel@micasaperu.com>
2026-04-04 20:01:45 -0500

    fix: pasar allPackages a PaymentFlow en PublicationFlow

--- components/PublicationFlow.tsx
+++ components/PublicationFlow.tsx
@@ -1056,6 +1056,7 @@ const PublicationFlow: React.FC<PublicationFlowProps> = ({
 
                             <PaymentFlow 
                               pkg={selectedPlan}
+                              allPackages={packages}
                               user={user}
                               paymentMethods={paymentMethods}
                               mpAccessToken={mpAccessToken}
